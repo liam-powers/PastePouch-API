f702b7e590998372f08750289ab35110c2053739
liam-powers <95184359+liam-powers@users.noreply.github.com>
2024-10-09 23:44:14 -0500

    feat: Gin wrapper + endpoints for all methods

--- main.go
+++ main.go
@@ -6,10 +6,14 @@ import (
 	"encoding/json"
 	"fmt"
 	"log"
+	"net/http"
 	"os"
+	"strconv"
 	"strings"
 
+	"github.com/gin-gonic/gin"
 	"github.com/joho/godotenv"
+
 	// for registering postgres as a driver for the sql connection
 	_ "github.com/lib/pq"
 )
@@ -32,9 +36,16 @@ func main() {
 
 	connectionType := -1
 	for connectionType != 0 && connectionType != 1 {
-		fmt.Print("Choose: \n(0) Local PostgreSQL\n(1) Supabase PostgreSQL\n")
-		fmt.Scan(&connectionType)
-		connectionType = int(connectionType)
+		fmt.Print("Choose: \n(0 / Default) Local PostgreSQL\n(1) Supabase PostgreSQL\n")
+
+		var input int
+		_, err = fmt.Scan(&input)
+		if err != nil {
+			fmt.Println("Defaulting to Local PostgreSQL")
+			connectionType = 0
+		} else {
+			connectionType = input
+		}
 	}
 
 	var connectionString string
@@ -56,6 +67,7 @@ func main() {
 		)
 	`)
 	checkErr(err)
+	fmt.Println("\nusers table OK")
 
 	_, err = db.Exec(`
 		CREATE TABLE IF NOT EXISTS pastes (
@@ -65,10 +77,48 @@ func main() {
 		)
 	`)
 	checkErr(err)
+	fmt.Println("pastes table OK")
+	fmt.Println("")
 
-	for true {
-		var val int
-		fmt.Print(`
+	interaction := -1
+
+	for interaction != 0 && interaction != 1 {
+		fmt.Print("Choose: \n(0 / Default) localhost:8080 endpoints\n(1) CLI interaction\n")
+
+		var input int
+		_, err = fmt.Scan(&input)
+		if err != nil {
+			fmt.Println("Defaulting to localhost:8080")
+			interaction = 0
+		} else {
+			interaction = input
+		}
+	}
+
+	if interaction == 0 {
+		fmt.Println("\nAs a reminder, you've made a Postman collection to quickly send HTTP requests.")
+		fmt.Println("")
+
+		router := gin.Default()
+		router.Use(dbMiddleware(db))
+
+		// following u/mcvoid1's URL vs Query vs JSON Parameters advice
+		// https://www.reddit.com/r/golang/comments/10huint/comment/j5b2tqv/?utm_source=share&utm_medium=web3x&utm_name=web3xcss&utm_term=1&utm_content=share_button
+		router.GET("/selectUsers", funcNameMiddleware("selectUsers"), ginExecuteSQL)
+		router.GET("/selectPastes", funcNameMiddleware("selectPastes"), ginExecuteSQL)
+		router.GET("/readPaste/:pasteid", funcNameMiddleware("readPaste"), ginExecuteSQL)
+
+		router.POST("/createUser", funcNameMiddleware("createUser"), ginExecuteSQL)
+		router.POST("/createPaste", funcNameMiddleware("createPaste"), ginExecuteSQL)
+
+		router.DELETE("/deletePaste/:pasteid", funcNameMiddleware("deletePaste"), ginExecuteSQL)
+		router.PUT("/updatePaste/:pasteid", funcNameMiddleware("updatePaste"), ginExecuteSQL)
+
+		router.Run("localhost:8080")
+	} else {
+		for true {
+			var val int
+			fmt.Print(`
 (1) Display all users entries
 (2) Display all pastes entries
 (3) Create a new user
@@ -77,89 +127,209 @@ func main() {
 (6) Delete a paste
 (7) Update a paste
 `)
-		fmt.Scan(&val)
+			fmt.Scan(&val)
 
-		switch val {
-		case 1:
-			rows, err := selectUsers(db)
-			checkErr(err)
-			json := rowsToJSON(rows)
-			fmt.Println(string(json))
-		case 2:
-			rows, err := selectPastes(db)
-			checkErr(err)
-			json := rowsToJSON(rows)
-			fmt.Println(string(json))
-		case 3:
-			var name string
-			var email string
-			fmt.Print("createUser> Enter name: ")
-			fmt.Scan(&name)
+			switch val {
+			case 1:
+				rows, err := selectUsers(db)
+				checkErr(err)
+				json := rowsToJSON(rows)
+				fmt.Println(string(json))
+			case 2:
+				rows, err := selectPastes(db)
+				checkErr(err)
+				json := rowsToJSON(rows)
+				fmt.Println(string(json))
+			case 3:
+				var name string
+				var email string
+				fmt.Print("createUser> Enter name: ")
+				fmt.Scan(&name)
 
-			fmt.Print("createUser> Enter email: ")
-			fmt.Scan(&email)
+				fmt.Print("createUser> Enter email: ")
+				fmt.Scan(&email)
 
-			_, err := createUser(db, name, email)
-			checkErr(err)
-		case 4:
-			var userid int
-			fmt.Print("createPaste> Enter userid: ")
-			fmt.Scan(&userid)
+				_, err := createUser(db, name, email)
+				checkErr(err)
+			case 4:
+				var userid int
+				fmt.Print("createPaste> Enter userid: ")
+				fmt.Scan(&userid)
 
-			fmt.Print("createPaste> Enter content: ")
-			in := bufio.NewReader(os.Stdin)
-			content, err := in.ReadString('\n')
-			checkErr(err)
-			// removing newline character from end (shows up as a + in pSQL)
-			content = strings.TrimSpace(content)
+				fmt.Print("createPaste> Enter content: ")
+				in := bufio.NewReader(os.Stdin)
+				content, err := in.ReadString('\n')
+				checkErr(err)
+				// removing newline character from end (shows up as a + in pSQL)
+				content = strings.TrimSpace(content)
 
-			fmt.Println("createPaste> Your content was: ", content)
+				fmt.Println("createPaste> Your content was: ", content)
 
-			_, err = createPaste(db, userid, content)
-			checkErr(err)
-		case 5:
-			var pasteid int
-			fmt.Print("readPaste> Enter pasteid: ")
-			fmt.Scan(&pasteid)
+				_, err = createPaste(db, userid, content)
+				checkErr(err)
+			case 5:
+				var pasteid int
+				fmt.Print("readPaste> Enter pasteid: ")
+				fmt.Scan(&pasteid)
 
-			rows, err := readPaste(db, pasteid)
-			checkErr(err)
-			json := rowsToJSON(rows)
-			fmt.Println(string(json))
-		case 6:
-			var pasteid int
-			fmt.Print("deletePaste> Enter pasteid: ")
-			fmt.Scan(&pasteid)
+				rows, err := readPaste(db, pasteid)
+				checkErr(err)
+				json := rowsToJSON(rows)
+				fmt.Println(string(json))
+			case 6:
+				var pasteid int
+				fmt.Print("deletePaste> Enter pasteid: ")
+				fmt.Scan(&pasteid)
 
-			rows, err := deletePaste(db, pasteid)
-			checkErr(err)
-			json := rowsToJSON(rows)
-			fmt.Println(string(json))
-		case 7:
-			var pasteid int
-			fmt.Print("updatePaste> Enter pasteid: ")
-			fmt.Scan(&pasteid)
+				rows, err := deletePaste(db, pasteid)
+				checkErr(err)
+				json := rowsToJSON(rows)
+				fmt.Println(string(json))
+			case 7:
+				var pasteid int
+				fmt.Print("updatePaste> Enter pasteid: ")
+				fmt.Scan(&pasteid)
 
-			fmt.Print("updatePaste> Enter content: ")
-			in := bufio.NewReader(os.Stdin)
-			content, err := in.ReadString('\n')
-			checkErr(err)
-			// removing newline character from end (shows up as a + in pSQL)
-			content = strings.TrimSpace(content)
+				fmt.Print("updatePaste> Enter content: ")
+				in := bufio.NewReader(os.Stdin)
+				content, err := in.ReadString('\n')
+				checkErr(err)
+				// removing newline character from end (shows up as a + in pSQL)
+				content = strings.TrimSpace(content)
 
-			fmt.Println("updatePaste> Your content was: ", content)
+				fmt.Println("updatePaste> Your content was: ", content)
 
-			rows, err := updatePaste(db, pasteid, content)
-			checkErr(err)
-			json := rowsToJSON(rows)
-			fmt.Println(string(json))
+				rows, err := updatePaste(db, pasteid, content)
+				checkErr(err)
+				json := rowsToJSON(rows)
+				fmt.Println(string(json))
+			}
 		}
 	}
+
 }
 
 func checkErr(err error) {
 	if err != nil {
 		log.Fatal(err)
+	}
+}
+
+func dbMiddleware(db *sql.DB) gin.HandlerFunc {
+	return func(c *gin.Context) {
+		c.Set("db", db)
+		c.Next()
+	}
+}
+
+func funcNameMiddleware(funcName string) gin.HandlerFunc {
+	return func(c *gin.Context) {
+		c.Set("funcName", funcName)
+		c.Next()
+	}
+}
+
+func ginExecuteSQL(c *gin.Context) {
+	db, exists := c.Get("db")
+	if !exists {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": "Database connection not found"})
+		return
+	}
+
+	dbConn, ok := db.(*sql.DB)
+	if !ok {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": "Database connection not of type database"})
+		return
+	}
+
+	funcName, exists := c.Get("funcName")
+	if !exists {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": "Function name not found"})
+		return
+	}
+
+	var rows *sql.Rows
+	var err error
+	switch funcName {
+	case "selectUsers":
+		rows, err = selectUsers(dbConn)
+	case "selectPastes":
+		rows, err = selectPastes(dbConn)
+	case "readPaste":
+		pasteidStr := c.Param("pasteid")
+		pasteid, err := strconv.Atoi(pasteidStr)
+		checkErr(err)
+
+		rows, err = readPaste(dbConn, pasteid)
+	case "createUser":
+		type RequestBody struct {
+			Name  string `json:"name"`
+			Email string `json:"email"`
+		}
+
+		var requestBody RequestBody
+		if err := c.BindJSON(&requestBody); err != nil {
+			c.JSON(http.StatusBadRequest, gin.H{"error": "Invalid request body"})
+			return
+		}
+
+		_, err = createUser(dbConn, requestBody.Name, requestBody.Email)
+	case "createPaste":
+		type RequestBody struct {
+			UserID  int    `json:"userid"`
+			Content string `json:"content"`
+		}
+
+		var requestBody RequestBody
+		if err := c.BindJSON(&requestBody); err != nil {
+			c.JSON(http.StatusBadRequest, gin.H{"error": "Invalid request body"})
+			return
+		}
+
+		_, err = createPaste(dbConn, requestBody.UserID, requestBody.Content)
+	case "deletePaste":
+		pasteidStr := c.Param("pasteid")
+		pasteid, err := strconv.Atoi(pasteidStr)
+		checkErr(err)
+
+		_, err = deletePaste(dbConn, pasteid)
+	case "updatePaste":
+		pasteidStr := c.Param("pasteid")
+		pasteid, err := strconv.Atoi(pasteidStr)
+		checkErr(err)
+
+		type RequestBody struct {
+			Content string `json:"content"`
+		}
+
+		var requestBody RequestBody
+
+		if err := c.BindJSON(&requestBody); err != nil {
+			c.JSON(http.StatusBadRequest, gin.H{"error": "Invalid request body"})
+			return
+		}
+
+		_, err = updatePaste(dbConn, pasteid, requestBody.Content)
+	default:
+		c.JSON(http.StatusInternalServerError, gin.H{"error": "Received a nonexistent funcName for executing SQL"})
+		return
+	}
+
+	checkErr(err)
+
+	if rows != nil {
+		rowsJSON := rowsToJSON(rows)
+
+		var result []map[string]interface{}
+		err = json.Unmarshal(rowsJSON, &result)
+		if err != nil {
+			c.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to parse JSON: " + err.Error()})
+			return
+		}
+
+		c.JSON(http.StatusOK, result)
+	} else {
+		c.JSON(http.StatusOK, "No rows to return.")
 	}
 }
 
@@ -195,20 +365,20 @@ func selectPastes(db *sql.DB) (*sql.Rows, error) {
 	return rows, err
 }
 
-func readPaste(db *sql.DB, pasteId int) (*sql.Rows, error) {
+func readPaste(db *sql.DB, pasteid int) (*sql.Rows, error) {
 	rows, err := db.Query(`
 		SELECT * FROM pastes
 		WHERE id=$1
-	`, pasteId)
+	`, pasteid)
 
 	return rows, err
 }
 
-func deletePaste(db *sql.DB, pasteId int) (*sql.Rows, error) {
+func deletePaste(db *sql.DB, pasteid int) (*sql.Rows, error) {
 	rows, err := db.Query(`
 		DELETE FROM pastes
 		WHERE id=$1
-	`, pasteId)
+	`, pasteid)
 
 	return rows, err
 }
